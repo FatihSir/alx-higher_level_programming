ef62deed7ba5fbf69214351ccb59e5a4fc189329
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-11-27 23:03:33 +0300

    fixes

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str.split(", ")[2].split()[0] + " " + str.split(", ")[2].split()[1] + " with python"
+str = str.split(", ")[2].split()[0] + " " + str.split(", ")[2].split()[1] +\
+ " with python"
 print(str)
