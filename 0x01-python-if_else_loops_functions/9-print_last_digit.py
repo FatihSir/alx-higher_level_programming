f275ff29a092a35cf231473021f104bfaff79cf0
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-11-29 20:31:50 +0300

    Task 9 fixes

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if type(number) == str:
-        return "Traceback (most recent call last)"
+        return "Traceback (most recent call last):"
     number = str(number)
     last_digit = number[-1:]
     print("{}".format(last_digit), end="")
