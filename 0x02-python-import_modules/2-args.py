962e2903a7d18da4aac3e134671f57fac5fb1b67
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-12-01 00:52:53 +0300

    Task 2 dixes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import sys
-length = len(sys.argv)
-if length == 1:
-    print("0 arguments.")
-elif length >= 2:
-    print("{} argument:".format(length - 1))
-    for i in range(1, length):
-        print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
+    length = len(sys.argv)
+    if length == 1:
+        print("0 arguments.")
+    elif length >= 2:
+        print("{} argument:".format(length - 1))
+        for i in range(1, length):
+            print("{}: {}".format(i, sys.argv[i]))
