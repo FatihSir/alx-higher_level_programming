0f326c9a08b3a5a03c2428a78fc3d0c9f25446c7
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-12-04 22:56:24 +0300

    Task 7 fixes

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,10 +5,14 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = tuple_a[0], 0
     elif length_a == 0:
         tuple_a = 0, 0
+    elif length_a > 2:
+        tuple_a = tuple_a[:2]
     elif length_b == 1:
         tuple_b = tuple_b[0], 0
     elif length_b == 0:
         tuple_b = 0, 0
+    elif length_b > 2:
+        tuple_b = tuple_b[:2]
     tuple_c = tuple(x + y for x, y in zip(tuple_a, tuple_b))
 
     return tuple_c
