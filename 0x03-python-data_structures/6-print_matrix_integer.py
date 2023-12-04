7d0260b54abe89c93c9ab534ae2c1299ad95879b
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-12-04 21:46:25 +0300

    Task 6 even more fixes

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if (matrix is None) or (matrix == [[]]):
+    if not matrix:
         print()
-        return
-    length, width = len(matrix), len(matrix[0])
-    for i in range(width):
-        for j in range(length):
-            if j == length - 1:
-                print("{:d}".format(matrix[i][j]))
-            else:
-                print("{:d}".format(matrix[i][j]), end=" ")
+    else:
+        length, width = len(matrix), len(matrix[0])
+        for i in range(width):
+            for j in range(length):
+                if j == length - 1:
+                    print("{:d}".format(matrix[i][j]))
+                else:
+                    print("{:d}".format(matrix[i][j]), end=" ")
