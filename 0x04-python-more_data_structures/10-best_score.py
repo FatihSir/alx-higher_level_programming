9f74b6cdeecc7dc35e49aef6dcfd064bdebf0f06
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-12-07 01:36:48 +0300

    fixes

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def best_score(my_dict):
-    if my_dict & len(my_dict):
+    if my_dict and len(my_dict):
         max_score = list(my_dict.keys())[0]
-        for keys in my_dict:
-            if my_dict[keys] > my_dict[max_score]:
-                max_score = keys
+        for key in my_dict:
+            if my_dict[key] > my_dict[max_score]:
+                max_score = key
         return max_score
     return None
