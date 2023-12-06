790989e26c089907878ebfe62abcf7b6a018712c
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-12-06 23:59:46 +0300

    fixes

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    if not set_1 or not set_2:
-        return None
-    com_elements = set_1 & set_2
+    com_elements = set(set_1 & set_2)
     return com_elements
