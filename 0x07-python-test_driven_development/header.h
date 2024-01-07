7d2c768a9d8f101d6c07385024409d13f08f5709
FatihSir <140240594+FatihSir@users.noreply.github.com>
2024-01-08 02:01:58 +0300

    Task 7 and 8

---
+++ 0x07-python-test_driven_development/header.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <wchar.h>
+#include <locale.h>
+#include <Python.h>
+#include <unicodeobject.h>
+
+void print_python_string(PyObject *p);
+
+#endif
