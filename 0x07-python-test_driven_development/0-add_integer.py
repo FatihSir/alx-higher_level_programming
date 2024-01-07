4be46e451821e3e3b7268d9b9e4072e0f79421fb
FatihSir <140240594+FatihSir@users.noreply.github.com>
2024-01-08 00:53:31 +0300

    Task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+""" A calculation module"""
+
+def add_integer(a, b=98):
+    """ A function that returns the sum of two integer variable
+
+    Parameters:
+    a (int, float): should be integer or float
+    b (int, float): should be integer or float
+
+    Returns:
+    int : The sum of the two entered arguments
+
+    """
+    if not isinstance(a,(int, float)):
+        raise TypeError('a must be an integer')
+    if not isinstance(b, (int, float)):
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
