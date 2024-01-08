a5d00b02813dccceb0fce7461eb04e6e307b994a
FatihSir <140240594+FatihSir@users.noreply.github.com>
2024-01-09 00:32:57 +0300

    Task 12 fixes

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,6 +3,8 @@
 
 
 def add_attribute(obj, name, value):
+    """ a function that adds a new attribute to an
+    object if it’s possible """
     if hasattr(obj, "__dict__") or \
        (hasattr(obj, "__slots__") and name in obj.__slots__):
         setattr(obj, name, value)
