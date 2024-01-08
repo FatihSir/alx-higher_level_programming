4498d5e915ef636eab39277fae533ba26aa61d0c
FatihSir <140240594+FatihSir@users.noreply.github.com>
2024-01-09 00:31:35 +0300

    Task 13

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""New feature module"""
+
+
+def add_attribute(obj, name, value):
+    if hasattr(obj, "__dict__") or \
+       (hasattr(obj, "__slots__") and name in obj.__slots__):
+        setattr(obj, name, value)
+    else:
+        raise TypeError("can't add new attribute")
