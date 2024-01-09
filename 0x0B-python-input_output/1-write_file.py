efb6fe27a11ea136ac96c12ec39038b1da960f7d
FatihSir <140240594+FatihSir@users.noreply.github.com>
2024-01-09 22:54:48 +0300

    Task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Write to a file module"""
+
+
+def write_file(filename="", text=""):
+    """ A function that writes a string to a text file
+    args:
+        filename (str): the name of the file to be created or write into
+        text (str): the text that will be added to the file
+    Returns:
+        The number of the written characters
+    """
+    with open(filename, mode="r+", encoding='utf-8') as f:
+        return f.write(text)
