51b33020f0b52c7c6a32dac6d402c0e7839d705b
FatihSir <140240594+FatihSir@users.noreply.github.com>
2024-01-09 22:39:55 +0300

    even more fixes

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,4 +10,4 @@ def read_file(filename=""):
         prints the content of the file to stdout
     """
     with open(filename, mode='r', encoding='utf-8') as f:
-        print(f.read())
+        print(f.read(), end="")
