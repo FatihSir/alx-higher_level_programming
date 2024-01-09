471bd50aea68d5d46c4bd029ce3db6df0492fd39
FatihSir <140240594+FatihSir@users.noreply.github.com>
2024-01-09 23:39:09 +0300

    Task 7 fixes

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,7 +2,6 @@
 """Load, add, save : adds all arguments to a Python list"""
 import sys
 
-
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
