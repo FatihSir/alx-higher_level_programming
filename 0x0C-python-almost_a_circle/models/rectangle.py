7c5173a7d7cf2aeebb5acc3d901c6de9f76b7c53
FatihSir <140240594+FatihSir@users.noreply.github.com>
2024-01-16 01:30:37 +0300

    Task 3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -60,3 +60,7 @@ class Rectangle(Base):
             raise ValueError("{} must be > 0".format(name))
         elif not distinct and v < 0:
             raise ValueError("{} must be >= 0".format(name))
+
+    def area(self):
+        """ Area calculator method"""
+        return self.__width * self.__height
