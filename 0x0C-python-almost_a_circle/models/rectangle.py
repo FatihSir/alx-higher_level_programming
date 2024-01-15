368d47968229d38704af2c529377b4dbd12ca9bd
FatihSir <140240594+FatihSir@users.noreply.github.com>
2024-01-16 02:28:56 +0300

    Task 7

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -67,8 +67,14 @@ class Rectangle(Base):
 
     def display(self):
         """ Rectangle Displaying Method """
+        if self.__y > 0:
+            for i in range(self.__y):
+                print()
+            self.__y = 0
         for i in range(self.__height):
             for j in range(self.__width):
+                if self.__y == j:
+                    print(" " * self.__x, end="")
                 print("#", end="")
             print()
 
