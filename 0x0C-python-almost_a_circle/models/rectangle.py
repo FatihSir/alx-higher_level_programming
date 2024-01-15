a8df569abd88b248049d8f7a723f1d74aa08c958
FatihSir <140240594+FatihSir@users.noreply.github.com>
2024-01-16 00:21:51 +0300

    Task 1

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" First Rectangle Module """
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ Rectangle class """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Rectangle's width"""
+        return self.__width
+
+    @width.setter
+    def width(self, v):
+        self.__width = v
+
+    @property
+    def height(self):
+        """Rectangle's height"""
+        return self.height
+
+    @height.setter
+    def height(self, v):
+        self.__height = v
+
+    @property
+    def x(self):
+        """Rectangle's x"""
+        return self.__x
+
+    @x.setter
+    def x(self, v):
+        self.__x = v
+
+    @property
+    def y(self):
+        """Rectangle's y"""
+        return self.__y
+
+    @y.setter
+    def y(self, v):
+        self.__y = v
