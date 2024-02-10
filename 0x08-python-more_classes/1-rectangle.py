56c59388b625d6f4b59d51cf5650e7ab041a0efa
FatihSir <140240594+FatihSir@users.noreply.github.com>
2024-02-10 09:45:34 +0300

    Task

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,16 +3,20 @@
 
 
 class Rectangle:
+    """Rectangle Class"""
     def __init__(self, width=0, height=0):
+    """Create new instance of the class"""
         self.height = height
         self.width = width
 
     @property
     def width(self):
+    """Retrive width value""" 
         return self.__width
 
     @width.setter
     def width(self, value=0):
+    """Set int non negative value"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -22,10 +26,12 @@ class Rectangle:
 
     @property
     def height(self):
+    """Retrive height value"""
         return self.__height
 
     @height.setter
     def height(self, value=0):
+    """set int non negative value"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
