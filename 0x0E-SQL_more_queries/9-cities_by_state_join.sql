6156be75b4c3a69333a54df11d40589e9bec1085
FatihSir <140240594+FatihSir@users.noreply.github.com>
2024-02-15 23:27:21 +0300

    Task 9 fixes

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,6 @@
 -- a script that lists all cities contained in the database
 SELECT cities.id, cities.name, states.name
-FROM hbtn_0d_usa
-ORDER BY cities.id ASCE;
+FROM cities AS c
+ON INNER JOIN hbtn_0d_usa.states AS s
+WHERE c.id = s.id
+ORDER BY c.id ASCE;
