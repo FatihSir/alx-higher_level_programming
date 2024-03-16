8ed37455be03395697b8511abc881f1fc21b8895
FatihSir <140240594+FatihSir@users.noreply.github.com>
2024-03-16 17:15:37 +0300

    Task 2 fixes

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
                          db=argv[3], s_name=argv[4], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = s_name ORDER BY id ASC")
+    cur.execute('SELECT * FROM states WHERE name = s_name ORDER BY id ASC')
     states = cur.fetchall()
     for state in states:
         print(state)
