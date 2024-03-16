cd0ae852f5fc8d87e936a2957949388b4dae9dfd
FatihSir <140240594+FatihSir@users.noreply.github.com>
2024-03-16 16:47:54 +0300

    Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" a script that lists all states with a name starting
-    with N (upper N) from the database hbtn_0e_0_usa """
+""" a script that lists all states with a name starting N """
 import MySQLdb
 from sys import argv
 
@@ -9,7 +8,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
                          db=argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE state LIKE 'N%'")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     states = cur.fetchall()
     for state in states:
         print(state)
