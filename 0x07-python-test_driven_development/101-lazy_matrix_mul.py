fd56b7db17561f397c988baff9c3491dd5a0bc30
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-04-21 17:26:41 +0300

    Task 101

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Lazy matrix multiplication"""
+
+
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """A function that multiplies 2 matrices by using the module NumPy
+
+    Args:
+        m_a (list of lists): the first matrix
+        m_b (list of lists): the second matrix
+
+    Return:
+        result (list of lists): the multiplication of
+        the first and the second matrix
+    """
+
+    result = np.dot(m_a, m_b)
+    return result
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/101-lazy_matrix_mul.txt", verbose=True)
