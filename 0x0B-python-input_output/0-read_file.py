284e87542f254ae491df6aeef92d22e949038039
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-05-05 21:35:37 +0300

    Readme and task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Read file Module"""
+
+
+def read_file(filename=""):
+    """
+    A function that reads a text file (UTF8) and prints it to stdout
+
+    Arguments:
+        filename (str): the name of the file to be read
+    """
+    with open(file=filename, mode='r', encoding='utf-8') as f:
+        text = f.read()
+        print(text)
