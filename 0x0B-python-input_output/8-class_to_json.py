8b5b2a8d3b6062e888fb6515c197da05cab99b97
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-05-06 20:25:49 +0300

    Fixes some bugs

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,20 +2,17 @@
 """ Class to JSON """
 
 
-import json
-
-
 def class_to_json(obj):
     """
-    A function that returns the dictionary description with
-    simple data structur (list, dictionary, string, integer
-    and boolean) for JSON serialization of an object:
+    A function that returns the dictionary description
+    with simple data structure (list, dictionary, string,
+    integer and boolean) for JSON serialization of an object:
 
     Arguments:
         obj (obj): is instance of a class
 
     Return:
-        dict_json (dict): the dictionary description of a class
+        my_dict (dict): the dictionary description of a class
     """
-    dict_json = json.dumps(obj.__dict__)
-    return dict_json
+    my_dict = obj.__dict__
+    return my_dict
