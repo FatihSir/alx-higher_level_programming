1ff5baa5989de77ac6fe3a6eed5ab451443dc09e
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-05-06 19:26:15 +0300

    Task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Load, add, save Module
+A script that adds all arguments to a Python list,
+and then save them to a file:
+"""
+
+
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+my_list = []
+if len(argv) < 2:
+    print("Nothing here")
+else:
+    for i in range(len(argv)):
+        if i == 0:
+            continue
+        my_list.append(argv[i])
+
+save_to_json_file(my_list, filename='add_item.json')
+my_obj = load_from_json_file(filename='add_item.json')
