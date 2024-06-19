977dd1bf6239e42f84799370cfc19ad7981cb463
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-06-19 03:00:51 +0300

    fixes

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,8 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-const list = process.argv.slice(2).map(Number).sort((a, b) => a - b);
+const args = process.argv.slice(2);
+const list = args.map(Number).sort((a, b) => a - b);
 console.log(list[list.length - 2]);
 }
 
