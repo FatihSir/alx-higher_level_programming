ce2fb57060349818bada249e229f95531f72e39d
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-06-20 00:28:07 +0300

    Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Square #1
+
+const OldSquare = require('./5-square');
+module.exports = class Square extends OldSquare {
+  charPrint (c) {
+    if (c === undefined) c = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};
