224845de986ef7b7a68f4676f83f1469ad208567
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-06-24 02:17:28 +0300

    Task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" SQL Injection """
+""" A script that lists all cities from the database hbtn_0e_4_usa """
 
 
 import MySQLdb
@@ -12,10 +12,12 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name = %s", [argv[4]])
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                ORDER BY cities.id ASC")
 
     rows = cur.fetchall()
     for row in rows:
-        print(i)
+        print(row)
     cur.close()
     db.close()
