c0e6f7db9368278f751722741722805f704684c0
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-06-24 02:39:50 +0300

    Task 10:

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" First state """
+""" Get a state """
 
 
 from model_state import Base, State
@@ -13,14 +13,12 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-    Session = sessionmaker()
-    session = Session(bind=engine)
-
+    Session = sessionmaker(bind=engine)
+    session = Session()
     Base.metadata.create_all(engine)
-    s_tate = session.query(State).order_by(State.id).first()
 
-    if s_tate:
-        print("{}: {}".format(s_tate.id, s_tate.name))
-    else:
-        print("Nothing")
+    s_tate = session.query(State).filter(State.name.like('%a%'))\
+                                 .order_by(State.id).all()
+    for state in s_tate:
+        print("{}: {}".format(state.id, state.name))
     session.close()
