e69f5e7362d9a915e99e4c0471affc25807bb00b
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-06-24 02:14:37 +0300

    Task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" a script that takes in an argument and displays all values in
-the states table of hbtn_0e_0_usa where name matches the argument. """
+""" SQL Injection """
 
 
 import MySQLdb
@@ -11,14 +10,12 @@ if __name__ == "__main__":
 
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
-    
+
     cur = db.cursor()
-    name = argv[4]
-    cur.execute("SELECT * FROM states WHERE name\
-                 LIKE BINARY '{}'".format(name))
+    cur.execute("SELECT * FROM states WHERE BINARY name = %s", [argv[4]])
 
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        print(i)
     cur.close()
     db.close()
