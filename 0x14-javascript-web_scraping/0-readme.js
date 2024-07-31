eb83803d57710c3f454916d46695d2ed21772bac
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-07-31 21:26:11 +0300

    semistandard fixes

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
-//A script that reads and prints the content of a file.
-
+// A script that reads and prints the content of a file.
 
 const fs = require('fs');
-fileName = process.argv[2];
+const fileName = process.argv[2];
 fs.readFile(fileName, 'utf8', (err, data) => {
-    if (err)
-    {
-        console.error(err);
-        return;
-    }
-    console.log(data);
-})
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
