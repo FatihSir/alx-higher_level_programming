b2b1ace311f30ddca5cd3738235372b0049789c6
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-07-31 21:22:38 +0300

    Task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/local/bin/node
+//A script that reads and prints the content of a file.
+
+
+const fs = require('fs');
+fileName = process.argv[2];
+fs.readFile(fileName, 'utf8', (err, data) => {
+    if (err)
+    {
+        console.error(err);
+        return;
+    }
+    console.log(data);
+})
