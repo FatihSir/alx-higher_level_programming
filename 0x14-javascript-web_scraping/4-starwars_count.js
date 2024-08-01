2095280e600416a3b6936508d70de941bd595a48
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-08-01 11:45:21 +0300

    ID count method improved

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,8 +2,8 @@
 // A script that display the status code of a GET request.
 
 const request = require('request');
-const requestURL = process.argv[2];
-const ID = 'https://swapi-api.alx-tools.com/api/people/18/';
+const requestURL = 'https://swapi-api.alx-tools.com/api/films';
+const ID = '18';
 let count = 0;
 
 request({ url: requestURL, json: true }, (error, response) => {
@@ -12,7 +12,7 @@ request({ url: requestURL, json: true }, (error, response) => {
   }
   for (const value of response.body.results) {
     for (const character of value.characters) {
-      if (ID === character) {
+      if (ID === character.split('/')[5]) {
         count += 1;
       }
     }
